342ec0584db29fa183e718e0682651c2f8a7c17c
Luiz T <90431370+LuizHTdS@users.noreply.github.com>
2023-03-10 13:56:36 -0300

    fixed FOR REAL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/LuizHTdS/notifications-page-frontend-mentor',
+  base: '/notifications-page-frontend-mentor',
   plugins: [react()],
 });
