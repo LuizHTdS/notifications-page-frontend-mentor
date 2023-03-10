67e00b3e4e9d5c8abafae094517df186c698bd8d
Luiz T <90431370+LuizHTdS@users.noreply.github.com>
2023-03-10 13:07:39 -0300

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/expenses-chart-component-main',
+  base: '/LuizHTdS/notifications-page-frontend-mentor',
   plugins: [react()],
 });
